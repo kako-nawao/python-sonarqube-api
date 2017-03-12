36155ec1e4f2a32998e4e23d29d3c224c77d592d
Claudio Omar Melendrez Baeza <claudio.melendrez@gmail.com>
2017-03-12 16:29:09 -0300

    Add python 3.6 to build, use codecov, update versions

--- setup.py
+++ setup.py
@@ -3,13 +3,12 @@ See:
 https://packaging.python.org/en/latest/distributing.html
 https://github.com/pypa/sampleproject
 """
-__author__ = 'kako'
-
 # Always prefer setuptools over distutils
 from setuptools import setup, find_packages
 # To use a consistent encoding
 from codecs import open
 from os import path
+
 
 here = path.abspath(path.dirname(__file__))
 
@@ -21,7 +20,7 @@ setup(
     name='sonarqube_api',
 
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='1.3.0',
+    version='1.3.1',
 
     description='SonarQube API Handler',
     long_description=long_description,
